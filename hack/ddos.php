cdcec211102e68b10c6e336cfdc6fd3ce3acded0
snplou <snplou@163.com>
2014-08-08 21:48:35 +0800

    dry the code,move header,sidebar,footer to other files

--- hack/ddos.php
+++ hack/ddos.php
@@ -14,30 +14,9 @@ auth();
     <title>quasiBot | ddos</title>
     <link href="../style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
+
 <body>
 <div id="wrapper">
-    <div id="header">
-
-
-        <div id="logo">
-            <h1><a href="../index.php">&nbsp; &nbsp; &nbsp;<?php echo date('H:i:s') ?></a></h1>
-
-<?php
-quotes();
-?>
-
-        </div>
-    </div>
-    <div id="menu">
-        <ul>
-            <li class="txt_left"><?php echo $_SERVER['DOCUMENT_ROOT'] ?></li>
-            <li class="txt_left"><?php echo $_SERVER['PHP_SELF'] ?></li>
-            <li class="txt_center"><?php echo date("d.m.y"); ?></li>
-            <li class="txt_right"><?php echo $_SERVER['SERVER_ADDR'] ?></li>
-            <li class="txt_right"><?php echo $_SERVER['SERVER_NAME'] ?></li>
-            <li class="txt_right"><?php echo conn('http://bot.whatismyipaddress.com/') ?></li>
-        </ul>
-    </div>
     <div id="page">
         <div id="page-bgtop">
             <div id="page-bgbtm">
@@ -54,6 +33,7 @@ quotes();
                             </p>
                         </div>
                     </div>
+
                     <div class="post">
                         <h2 class="title"><a href="#">DDoS</a></h2>
                         <div class="entry">
@@ -127,56 +107,10 @@ quotes();
                     </div>
                     <div style="clear: both;">&nbsp;</div>
                 </div>
-                <div id="sidebar">
-                    <ul>
-                        <li>
-                            <h2>Home</h2>
-                            <ul>
-                                <li><a href="../home/index.php">Index</a></li>
-                                <li><a href="../home/settings.php">Settings</a></li>
-                                <li><a href="../home/rss.php">RSS</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                           <h2>Hack</h2>
-                            <ul>
-                                <li><a href="rce.php">RCE</a></li>
-                                <li><a href="scan.php">Scan</a></li>
-                                <li><a href="pwn.php">Pwn</a></li>
-                                 <li><a href="shell.php">Shell</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                           <h2>Bruteforce</h2>
-                            <ul>
-                                <li><a href="../bf/ssh.php">SSH</a></li>
-                                <li><a href="../bf/ftp.php">FTP</a></li>
-                                <li><a href="../bf/dbs.php">DB's</a></li>
-                                <li><a href="../bf/www.php">WWW</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                           <h2>Tools</h2>
-                            <ul>
-                                <li><a href="../tools/sql.php">MySQL Manager</a></li>
-                                <li><a href="../tools/hostscan.php">HostScan</a></li>
-                             </ul>
-                        </li>
-                        <li>
-                           <h2>Bots</h2>
-                            <ul>
-                                <li><a href="ddos.php"><b>DDoS</b></a></li>
-                                <li><a href="more.php">Run</a></li>
-                             </ul>
-                        </li>
-                </div>
                 <div style="clear: both;">&nbsp;</div>
             </div>
         </div>
     </div>
-</div>
-<div id="footer">
-    <p><?php echo $_SERVER['SERVER_SOFTWARE']; echo '  -  '; echo $_SERVER['HTTP_USER_AGENT'] ?></p>
 </div>
 </body>
 </html>
